2336f6fc35bacd5eaff82291e1ab63e825cfb65d
roboken-dev <ckunkel@hobokencs.net>
2020-10-22 15:59:51 -0400

    Adding sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/githubtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/githubtest.java
@@ -1,4 +1,82 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class githubtest {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+public class githubtest extends LinearOpMode{
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    @Override
+
+
+    public void runOpMode()
+    {
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        motorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        double DRIVE_POWER = 0.5;
+
+
+        waitForStart();
+
+
+
+        DriveForwardByTime(DRIVE_POWER,1000);
+
+        TurnRightByTime(DRIVE_POWER,1000);
+
+        DriveForwardByTime(DRIVE_POWER,750);
+
+        TurnLeftByTime(DRIVE_POWER,1000);
+
+        DriveForwardByTime(DRIVE_POWER,1000);
+
+
+    }
+
+
+    public void DriveForwardByTime(double power, long time)
+    {
+        motorLeft.setPower(power);
+        motorRight.setPower(power);
+        sleep(time);
+
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        sleep(250);
+    }
+
+
+    public void TurnRightByTime(double power, long time)
+    {
+        motorLeft.setPower(power);
+        motorRight.setPower(-power);
+        sleep(time);
+
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        sleep(250);
+    }
+
+
+    public void TurnLeftByTime(double power, long time)
+    {
+        motorLeft.setPower(-power);
+        motorRight.setPower(power);
+        sleep(time);
+
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        sleep(250);
+    }
+
+
+
 }
